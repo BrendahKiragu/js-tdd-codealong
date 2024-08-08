246f8333fbcf18f6d98f1da44a3b81866c461016
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-08 22:00:48 +0300

    write a test and implement it

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,15 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    //arrange
+    const currentYear = new Date().getFullYear()
     const birthYear = 1984;
+    const answer = currentYear - birthYear;
+
+    //act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    //assert
+    expect(ageOfPerson).toBe(answer);
   });
 });
